7a4647a5fb5ba7cafce059f2fdc45bd837fecbd2
BurhanHelmy-tech <buhanhelmy44@gmail.com>
2025-04-15 00:04:40 +0800

    Add code for quiz website

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Jumlah sudut pedalaman segitiga",
+        answer: "B. 180",
+        options: [
+            "A. 190",
+            "B. 180",
+            "C. 170",
+            "D. 150"
+        ]
+    },
+    {
+        numb: 2,
+        question: "2a + 2a + 2a = ",
+        answer: "A. 6a",
+        options: [
+            "A. 6a",
+            "B. 6a⁴",
+            "C. 8a",
+            "D. 8a⁴"
+        ]
+    },
+    {
+        numb: 3,
+        question: "2a x 2a x 2a x 2a = ",
+        answer: "D. 16a⁴",
+        options: [
+            "A. 8a⁴",
+            "B. 8a",
+            "C. 16a",
+            "D. 16a⁴"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Faktorkan x² + 5x + 6",
+        answer: "D. (x + 3)(x + 2)",
+        options: [
+            "A. (x + 5)(x+1)",
+            "B. (x + 4)(x + 2)",
+            "C. (x + 6)(x + 1)",
+            "D. (x + 3)(x + 2)"
+        ]
+    },
+    {
+        numb: 5,
+        question: "2a + 2 = 10, Cari a.",
+        answer: "C. 4",
+        options: [
+            "A. 12",
+            "B. 8",
+            "C. 4",
+            "D. 6"
+        ]
+    },
+];
